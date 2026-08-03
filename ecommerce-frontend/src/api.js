ecbc1bc1c4b6274abf4d37fe973729c713f6ee75
Sunkari Maheswari <sunkarimaheswari123@gmail.com>
2026-08-03 09:18:38 +0530

    Point frontend to live backend URL

--- ecommerce-frontend/src/api.js
+++ ecommerce-frontend/src/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://ecommerce-analytics-dashboard-r14t.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-// ప్రతి request తో Token పంపడానికి (login అయి ఉంటే)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('authToken');
   if (token) {
